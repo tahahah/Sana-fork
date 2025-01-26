3aa8be79a77cc20a84b59dc9b8fff888f45eaafb
Taha Ansari <tahaaansari@hotmail.com>
2025-01-26 16:11:59 +1100

    fix: param name change

--- diffusion/model/nets/pacman_diffusion.py
+++ diffusion/model/nets/pacman_diffusion.py
@@ -52,7 +52,7 @@ class PacmanDiffusionModel(nn.Module):
         self.history_encoder = build_history_encoder(
             in_channels=3 * seq_length,  
             out_channels=3,  
-            hidden_channels=3 * seq_length // 2  
+            hidden_dim=3 * seq_length // 2  
         )
         
         # Create Sana model for diffusion with latent input channels from VAE
