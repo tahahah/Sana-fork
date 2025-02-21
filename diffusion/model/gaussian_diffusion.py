10f9172bab5fba45fa233b649b7b5be54671a332
Taha Ansari <tahaaansari@hotmail.com>
2025-02-21 23:26:19 +1100

    fix: unecessary removal of code

--- diffusion/model/gaussian_diffusion.py
+++ diffusion/model/gaussian_diffusion.py
@@ -337,24 +337,9 @@ class GaussianDiffusion:
         else:
             extra = None
 
-        if isinstance(model_output, dict) and model_output.get("x", None) is not None:
-            output = model_output["x"]
-        else:
-            output = model_output
-
-        if self.return_startx and self.model_mean_type == ModelMeanType.EPSILON:
-            B, C = x.shape[:2]
-            assert output.shape == (B, C * 2, *x.shape[2:])
-            output = th.split(output, C, dim=1)[0]
-            return output, self._predict_xstart_from_eps(x_t=x, t=t, eps=output), x
-
-        if self.model_var_type in [
-            ModelVarType.LEARNED,
-            ModelVarType.LEARNED_RANGE,
-        ]:
-            B, C = x.shape[:2]
-            assert output.shape == (B, C * 2, *x.shape[2:])
-            output, model_var_values = th.split(output, C, dim=1)
+        if self.model_var_type in [ModelVarType.LEARNED, ModelVarType.LEARNED_RANGE]:
+            assert model_output.shape == (B, C * 2, *x.shape[2:])
+            model_output, model_var_values = th.split(model_output, C, dim=1)
             min_log = _extract_into_tensor(self.posterior_log_variance_clipped, t, x.shape)
             max_log = _extract_into_tensor(np.log(self.betas), t, x.shape)
             # The model_var_values is [-1, 1] for [min_var, max_var].
@@ -377,8 +362,8 @@ class GaussianDiffusion:
             model_variance = _extract_into_tensor(model_variance, t, x.shape)
             model_log_variance = _extract_into_tensor(model_log_variance, t, x.shape)
         else:
-            model_variance = th.zeros_like(output)
-            model_log_variance = th.zeros_like(output)
+            model_variance = th.zeros_like(model_output)
+            model_log_variance = th.zeros_like(model_output)
 
         def process_xstart(x):
             if denoised_fn is not None:
@@ -388,9 +373,9 @@ class GaussianDiffusion:
             return x
 
         if self.model_mean_type == ModelMeanType.START_X:
-            pred_xstart = process_xstart(output)
+            pred_xstart = process_xstart(model_output)
         else:
-            pred_xstart = process_xstart(self._predict_xstart_from_eps(x_t=x, t=t, eps=output))
+            pred_xstart = process_xstart(self._predict_xstart_from_eps(x_t=x, t=t, eps=model_output))
         model_mean, _, _ = self.q_posterior_mean_variance(x_start=pred_xstart, x_t=x, t=t)
 
         assert model_mean.shape == model_log_variance.shape == pred_xstart.shape == x.shape
@@ -801,11 +786,6 @@ class GaussianDiffusion:
                 terms["loss"] *= self.num_timesteps
         elif self.loss_type == LossType.MSE or self.loss_type == LossType.RESCALED_MSE:
             model_output = model(x_t, t, **model_kwargs)
-            if isinstance(model_output, tuple):
-                model_output, extra = model_output
-            else:
-                extra = None
-
             if isinstance(model_output, dict) and model_output.get("x", None) is not None:
                 output = model_output["x"]
             else:
@@ -929,11 +909,6 @@ class GaussianDiffusion:
                 terms["loss"] *= self.num_timesteps
         elif self.loss_type == LossType.MSE or self.loss_type == LossType.RESCALED_MSE:
             output = model(x_t, timestep=t, **model_kwargs, return_dict=False)[0]
-
-            if isinstance(output, dict) and output.get("x", None) is not None:
-                output = output["x"]
-            else:
-                output = output
 
             if self.return_startx and self.model_mean_type == ModelMeanType.EPSILON:
                 B, C = x_t.shape[:2]
@@ -1063,6 +1038,7 @@ class GaussianDiffusion:
                     x_t=x_t,
                     t=t_batch,
                     clip_denoised=clip_denoised,
+                    model_kwargs=model_kwargs,
                 )
             vb.append(out["output"])
             xstart_mse.append(mean_flat((out["pred_xstart"] - x_start) ** 2))
