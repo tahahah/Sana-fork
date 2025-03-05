08364e3509df53fcc0e90daf3f81452aed33c225
Taha Ansari <tahaaansari@hotmail.com>
2025-03-05 11:53:17 +1100

    fix: config key error

--- inference_pacman.py
+++ inference_pacman.py
@@ -101,7 +101,7 @@ def setup_model(config, checkpoint_path=None, device='cuda', debug=False):
         print(model)
     
     # Load checkpoint if provided
-    ckpt_path = osp.join(config.work_dir, "checkpoints")
+    ckpt_path = osp.join(config.train.work_dir, "checkpoints")
     check_flag = osp.exists(ckpt_path) and len(os.listdir(ckpt_path)) != 0
     if config.model.resume_from["checkpoint"] == "latest":
         if check_flag:
