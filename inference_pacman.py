faa00dd345c5967d7a4f1b397541b783cee573ca
Taha Ansari <tahaaansari@hotmail.com>
2025-03-05 15:10:09 +1100

    fix: lesser steps

--- inference_pacman.py
+++ inference_pacman.py
@@ -437,7 +437,7 @@ def run_pacman_inference(config, args):
             # Run the sampling
             denoised = dpm_solver.sample(
                 z,
-                steps=20,
+                steps=10,
                 order=2,
                 skip_type="time_uniform_flow",
                 method="multistep",
