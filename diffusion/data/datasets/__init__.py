b12590bbb1ddb1c50faed06c886c73a507d24825
Taha Ansari <tahaaansari@hotmail.com>
2025-05-26 02:03:11 +1000

    fix: init PacmanDatasetSimple

--- diffusion/data/datasets/__init__.py
+++ diffusion/data/datasets/__init__.py
@@ -1,4 +1,5 @@
 from .sana_data import SanaImgDataset, SanaWebDataset
 from .sana_data_multi_scale import DummyDatasetMS, SanaWebDatasetMS
 from .pacman_data import PacmanDataset, PacmanDatasetMS
+from .pacman_data_simple import PacmanDatasetSimple
 from .utils import *
